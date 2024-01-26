4390b4eb992ef7370e361c07066f54ed3fa13d7a
MRNSJ26 <jijjinoor@gmail.com>
2024-01-26 19:51:30 +0300

    learning about the useReducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 function reducer(state, action) {
   if (action.type === "inc") return state + 1;
@@ -8,8 +8,10 @@ function reducer(state, action) {
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  // const [step, setStep] = useState(1);
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, 0);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -35,12 +37,12 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
